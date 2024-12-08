b49753b52e61d57d3d3604bcb4021819ef6ebebd
eXory2024 <exory@anio.software>
2024-12-08 15:59:25 +0100

    Update fourtune.config.mjs.

--- fourtune.config.mjs
+++ fourtune.config.mjs
@@ -1,17 +1,6 @@
-import {generateFromTemplate} from "fourtune/autogenerate"
-
-const asyncToSync = {
-	"import {mkdir} from \"@anio-fs/api/async\"": "import {mkdir} from \"@anio-fs/api/sync\"",
-	"export default async function(": "export default function(",
-	"await mkdir": "mkdir"
-}
-
 export default {
-	realm: "js",
-	type: "package",
-
-	autogenerate: {
-		"sync.mjs": generateFromTemplate("src/template.mjs", asyncToSync),
-		"async.mjs": generateFromTemplate("src/template.mjs", {})
+	realm: {
+		name: "js",
+		type: "package"
 	}
 }
