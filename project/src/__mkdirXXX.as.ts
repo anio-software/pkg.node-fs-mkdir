d19b0e7d0fb927e066936012a30726280cb98b82
eXory2024 <exory@anio.software>
2025-07-01 21:10:14 +0200

    Add empty implementation of mkdir and mkdirp.

---
+++ project/src/__mkdirXXX.as.ts
@@ -0,0 +1,17 @@
+import {
+	type EnkoreJSRuntimeContextOptions,
+	createContext
+} from "@anio-software/enkore.js-runtime"
+
+import type {MakeDirectoryOptions as Options} from "#~export/MakeDirectoryOptions.ts"
+//>import type {MakeDirectorySyncOptions as Options} from "#~export/MakeDirectorySyncOptions.ts"
+
+export async function __implementation(
+//>export function __implementationSync(
+	contextOptions: EnkoreJSRuntimeContextOptions,
+	pathToCreate: string,
+	options?: Options|undefined
+): Promise<boolean> {
+//>): boolean {
+	return false
+}
---
+++ project/src/__mkdirpXXX.as.ts
@@ -0,0 +1,17 @@
+import {
+	type EnkoreJSRuntimeContextOptions,
+	createContext
+} from "@anio-software/enkore.js-runtime"
+
+import type {MakeDirectoryPOptions as Options} from "#~export/MakeDirectoryPOptions.ts"
+//>import type {MakeDirectoryPSyncOptions as Options} from "#~export/MakeDirectoryPSyncOptions.ts"
+
+export async function __implementation(
+//>export function __implementationSync(
+	contextOptions: EnkoreJSRuntimeContextOptions,
+	pathToCreate: string,
+	options?: Options|undefined
+): Promise<boolean> {
+//>): boolean {
+	return false
+}
