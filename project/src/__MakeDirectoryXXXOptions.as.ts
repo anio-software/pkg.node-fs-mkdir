372cd839850a301b70d8192a1d7b161be94bb357
eXory2024 <exory@anio.software>
2025-07-01 21:05:57 +0200

    Use common options.

--- project/src/__MakeDirectoryXXXOptions.as.ts
+++ project/src/__MakeDirectoryXXXOptions.as.ts
@@ -1,4 +1,6 @@
-export type __XX__ = {
+import type {MakeDirectoryCommonOptions as CommonOptions} from "#~export/MakeDirectoryCommonOptions.ts"
+//>import type {MakeDirectorySyncCommonOptions as CommonOptions} from "#~export/MakeDirectorySyncCommonOptions.ts"
+
+export type __XX__ = CommonOptions & {
 	recursive?: boolean
-	mode?: number|undefined
 }
